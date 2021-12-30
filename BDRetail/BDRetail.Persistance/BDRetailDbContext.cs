f69588bf4bbe92bb0f856c508c7491cfd7d84c1f
Bojidar Dimitrov <bojidar.dimitrov00@gmail.com>
2021-12-30 13:54:00 +0200

    Add DbContext

---
+++ BDRetail/BDRetail.Persistance/BDRetailDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BDRetail.Persistance
+{
+    public class BDRetailDbContext : IdentityDbContext
+    {
+        public BDRetailDbContext(DbContextOptions<BDRetailDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
